5edf09c1e273865c634b97ee120bd0fb48e736ae
Tomas Zubik <zubik.tomas@gmail.com>
2023-10-08 18:56:57 -0300

    feat: :art: refactor create transaction template and scripts

--- src/bank_of_kukelia/entrypoints/api/static/js/main.js
+++ src/bank_of_kukelia/entrypoints/api/static/js/main.js
@@ -86,22 +86,6 @@
   }
 
   /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
    * Initiate tooltips
    */
   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
---
+++ src/bank_of_kukelia/entrypoints/api/static/js/transaction.js
@@ -0,0 +1,48 @@
+const bearerToken = localStorage.getItem('access_token');
+
+let form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    submitForm();
+});
+
+function submitForm() {
+    const formData = {};
+    // Loop through the form's elements and populate the formData object
+    for (const element of form.elements) {
+        if (element.name && element.tagName === 'INPUT') {
+            formData[element.name] = element.value;
+        }
+    }
+    motive = document.getElementsByTagName('textarea')[0];
+    formData['motive'] = motive.value;
+    window.alert(formData);
+    // window.location.href is the current page's URL
+    fetch('/dashboard/transaction', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => {
+        response.json().then(data => {
+            if (response.ok) {
+                // Handle the redirect response
+                window.alert(data.message);
+                window.location.href = `/dashboard?token=${bearerToken}`;
+            }else {
+                // Handle other responses (e.g., errors)
+                window.alert(data.detail);
+                console.log(response);
+                console.error('Error:', response.status);
+                throw new Error('Request failed with status ' + response.status);
+            }
+        })
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+}
