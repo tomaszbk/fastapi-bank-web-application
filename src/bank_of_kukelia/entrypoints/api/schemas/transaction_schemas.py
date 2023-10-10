458eba3764e3a70381dcc7b813d4bc8d1bbe2e09
Tomas Zubik <zubik.tomas@gmail.com>
2023-10-09 22:33:50 -0300

    fix: :bug: deprecated pydantic function

--- src/bank_of_kukelia/entrypoints/api/schemas/transaction_schemas.py
+++ src/bank_of_kukelia/entrypoints/api/schemas/transaction_schemas.py
@@ -1,10 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class TransactionCreate(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     amount: float
     destiny_username: str
     description: str | None = None
-
-    class Config:
-        from_attributes = True
