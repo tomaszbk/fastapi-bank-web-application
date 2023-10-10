92c2328774c062dca901b6d3ffa92f0d96b65ef7
Tomas Zubik <zubik.tomas@gmail.com>
2023-10-09 21:08:02 -0300

    refactor: :art: migrated pydantic @validator to @field_validator

--- src/bank_of_kukelia/entrypoints/api/schemas/user_schemas.py
+++ src/bank_of_kukelia/entrypoints/api/schemas/user_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 
 
 class UserCreate(BaseModel):
@@ -10,19 +10,19 @@ class UserCreate(BaseModel):
     age: int
     email: str
 
-    @validator("age")
+    @field_validator("age")
     def age_must_be_greater_than_18(cls, age):
         if age < 18 or age > 100:
             raise ValueError("Age must be greater than 18 or less than 100")
         return age
 
-    @validator("dni")
+    @field_validator("dni")
     def dni_must_be_valid(cls, dni):
         if len(str(dni)) != 8:
             raise ValueError("DNI must be 8 digits long")
         return dni
 
-    @validator("email")
+    @field_validator("email")
     def email_must_be_valid(cls, email):
         if "@" not in email:
             raise ValueError("Email must be valid")
