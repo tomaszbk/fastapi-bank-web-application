54c25e0cfbd631ad0adc9a7423dbba79ef13c7ed
tomasz <zubik.tomas@gmail.com>
2023-11-02 10:31:41 -0300

    docs: :memo: added presentation files

--- presentation/app.py
+++ presentation/app.py
@@ -1,21 +1,44 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import redis
 import requests
+import json
 
-
+# port 8000
 app = FastAPI()
 redis_client = redis.Redis(host="localhost", port=6379)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    if redis_client.exists("weather"):
+        weather = redis_client.get("weather").decode("utf-8")
+    else:
+        response = requests.get("http://localhost:5000/get_weather")
+        response.raise_for_status()
+        weather = json.loads(response.text)
+        redis_client.set("weather", weather)
+    return f"""
+        <html>
+            <head><title>Weather</title></head>
+            <body>
+                <h1>Weather is {weather}</h1>
+            </body>
+        </html>"""
 
 
-@app.get("/zodiac_sign/{date}")
-async def get_from_redis(date: int):
-    if redis_client.exists(date):
-        return {"zodiac_sign": redis_client.hget(date)}
-    zodiac_sign = requests.get(f"http://localhost:5000/zodiac/{date}")
-    redis_client.hset(date, zodiac_sign)
-    return {"message": f"Your zodiac sign is {zodiac_sign}"}
+@app.post("/validate_user/{username}")
+async def validate_user(username: str):
+    if redis_client.exists(f"user:{username}", "validated"):
+        if redis_client.hget(f"user:{username}", "validated") is True:
+            return "user is validated"
+        return "user not validated"
+    else:
+        response = requests.get(f"http://localhost:5000/validate_user/{username}")
+        response.raise_for_status()
+        state = response.text
+        redis_client.hset(f"user:{username}", "validated", state)
+        redis_client.expire(f"user:{username}", 60)
+        if state == "true":
+            return "user is validated"
+        return "user not validated"
