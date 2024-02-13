e70d347bce644037b4c0b941925241a77d34ef18
tomaszbk-pc <zubik.tomas@gmail.com>
2024-02-13 16:01:46 -0300

    refactor: :sparkles: updated docs and configs

--- app/infrastructure/engine.py
+++ app/infrastructure/engine.py
@@ -19,15 +19,8 @@ class PostgresSessionFactory:
             logger.info("session closed")
             session.close()
 
-
-class PostgresLocalSessionFactory:
-    def __init__(self) -> None:
-        self.engine = create_engine(get_postgres_uri())
-        self.Session = sessionmaker(bind=self.engine)
-
-    def get_session(self):
+    def get_session_no_yield(self):
         return self.Session()
 
 
 postgres_session_factory = PostgresSessionFactory()
-postgres_local_session_factory = PostgresLocalSessionFactory()
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,1 @@
-alembic upgrade head
 python -m uvicorn main:app --host 0.0.0.0 --port 8000
